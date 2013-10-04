5475951e59076b75ba5c20c5c8008ee0c58ea96a
Medina87 <duck.medina38@gmail.com>
2013-10-03 20:24:55 -0500

    Signed-off-by: Medina87 <duck.medina38@gmail.com>

---
+++ ciudades.php
@@ -0,0 +1,26 @@
+<?php
+ 
+$Mexico = array ('Guadalajara','Monterrey','Toluca','Mexicali','Ciudad de Mexico');
+  echo "Las principales ciudades  de Mexico son <br />";
+  for($i = 0; $i <=5; $i++) 
+  echo $Mexico[$i]."<br />";
+$Inglaterra = array ('Londres','Liverpool','Manchester','Birmingham','York');
+  echo "Las principales ciudades  de Inglaterra son <br />";
+  for($i = 0; $i <=5; $i++) 
+  echo $Inglaterra[$i]."<br />";
+$Japon = array ('Osaka','Kyoto','Hiroshima','Tokyo','Okinawa');
+  echo "Las principales ciudades  de Japon son <br />";
+  for($i = 0; $i <=5; $i++) 
+  echo $Japon[$i]."<br />";
+$Italia = array ('Napoles','Florencia','Venecia','Roma','Palermo');
+  echo "Las principales ciudades  de Italia son <br />";
+  for($i = 0; $i <=5; $i++) 
+  echo $Italia[$i]."<br />";
+
+$España = array ('Madrid','Barcelona','Valencia','Sevilla','Zaragoza');
+  echo "Las principales ciudades  de España son <br />";
+  for($i = 0; $i <=5; $i++) 
+  echo $España[$i]."<br />";
+
+
+?>
